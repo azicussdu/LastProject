5ff647d353482b00131520fb3db99ebec4ff1fc2
Azamat <azicus.sdu@gmail.com>
2024-01-11 02:28:36 +0600

    Login class

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First line");
     }
+
+    public static void doLogin(){
+        Login log = new Login();
+        log.doLogin();
+    }
 }
